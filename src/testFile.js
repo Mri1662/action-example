249e5b70120c4deb95b3da2ce3dfbd7bf4ee73f9
Mansi Italiya <mansiitaliya88@gmail.com>
2023-09-24 17:08:21 +0530

    mansi - 24-09-23

--- src/testFile.js
+++ src/testFile.js
@@ -2,3 +2,34 @@ module.exports = {
   add: (a, b) => a + b,
   subtract: (a, b) => a - b,
 }
+
+const sum = (a, b) => {
+  return a+b;
+};
+
+// Missing semicolon and extra spacing
+const multiply = (a, b) => { return a * b }
+
+// Unused variable 'c'
+const unusedVar = (a, b, c) => {
+  return a + b;
+};
+
+// '==' should be '==='
+if (sum(1, 2) == 3) {
+  console.log('This is not type-safe.');
+}
+
+// Trailing whitespace at the end of the line
+const divide = (a, b) => {  
+  if (b === 0) {
+    return 'Cannot divide by zero.';
+  }
+  return a / b;
+};
+
+// 'var' instead of 'let' or 'const'
+var outdatedVar = "I'm an outdated variable declaration";
+
+// Global variable without declaration
+undeclaredVar = 'I am undeclared';
