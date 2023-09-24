f6097efa5c2ae9dd199e84b0b15e3f5536f1284a
Mansi Italiya <mansiitaliya88@gmail.com>
2023-09-24 16:16:26 +0530

    commit

--- __test__/testFile.test.js
+++ __test__/testFile.test.js
@@ -2,18 +2,18 @@ const { add, subtract, divide, multiply } = require("../src/testFile");
 
 describe("Math.js tests", () => {
   it("should test  add  ", () => {
-    expect(add(2, 2)).toBe(4);
+    expect(add(5, 5)).toBe(10);
   });
 
   it("Should test subtract", () => {
-    expect(subtract(2, 2)).toBe(0);
+    expect(subtract(10, 5)).toBe(5);
   });
 
   it("Should test divide", () => {
-    expect(divide(2, 2)).toBe(1);
+    expect(divide(10, 2)).toBe(5);
   });
 
   it("Should test multiply", () => {
-    expect(multiply(2, 2)).toBe(4);
+    expect(multiply(5, 5)).toBe(25);
   });
 });
