1775912767f946a57d2db27df37b6c79033a4b78
Mansi Italiya <mansiitaliya88@gmail.com>
2023-09-24 17:19:57 +0530

    mansi - 24-09-23

--- src/testFile.js
+++ src/testFile.js
@@ -4,32 +4,32 @@ module.exports = {
 }
 
 const sum = (a, b) => {
-  return a+b;
-};
+  return a+b
+}
 
 // Missing semicolon and extra spacing
-const multiply = (a, b) => { return a * b }
+// const multiply = (a, b) => { return a * b }
 
 // Unused variable 'c'
-const unusedVar = (a, b, c) => {
-  return a + b;
-};
+// const unusedVar = (a, b, c) => {
+//   return a + b;
+// };
 
 // '==' should be '==='
-if (sum(1, 2) == 3) {
-  console.log('This is not type-safe.');
+if (sum(1, 2) === 3) {
+  console.log('This is not type-safe.')
 }
 
 // Trailing whitespace at the end of the line
-const divide = (a, b) => {  
-  if (b === 0) {
-    return 'Cannot divide by zero.';
-  }
-  return a / b;
-};
+// const divide = (a, b) => {  
+//   if (b === 0) {
+//     return 'Cannot divide by zero.'
+//   }
+//   return a / b;
+// };
 
 // 'var' instead of 'let' or 'const'
-var outdatedVar = "I'm an outdated variable declaration";
+// let outdatedVar = "I'm an outdated variable declaration";
 
 // Global variable without declaration
-undeclaredVar = 'I am undeclared';
+// undeclaredVar = 'I am undeclared';
